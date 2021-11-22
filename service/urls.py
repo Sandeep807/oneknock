0282290a0041e104de7319e6c7de8729373d77bd
sandeep <sandeepkp601@gmail.com>
2021-11-22 18:28:26 +0530

    sandeep

--- service/urls.py
+++ service/urls.py
@@ -3,5 +3,5 @@ from .views import *
 urlpatterns = [
     path('service/',ServiceView.as_view()),
     path('sub_service/',ServiceSubCategoryView.as_view()),
-    path('bookingservice/',ServiceSubCategoryView.as_view()),
+    path('bookingservice/',BookingServiceView.as_view()),
 ]
